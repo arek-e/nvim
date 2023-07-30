af4fde1e8dc31f1d4947849f05cacd71fa9143f7
Alexander Eklund <alexeklund460@gmail.com>
2023-07-30 10:27:45 +0200

    Add onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -15,11 +15,22 @@ return {
     end,
   },
 
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        style = "deep",
+      })
+    end,
+  },
+
   -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "bluloco",
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
 }
